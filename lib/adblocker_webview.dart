81880ca37a55080740b71f2e27975083673cdae2
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2026-03-02 00:56:09 -0400

    exposed injected scripts

--- lib/adblocker_webview.dart
+++ lib/adblocker_webview.dart
@@ -2,3 +2,5 @@ export 'package:adblocker_manager/adblocker_manager.dart';
 export 'src/adblocker_webview.dart';
 export 'src/adblocker_webview_controller.dart';
 export 'src/adblocker_webview_controller_impl.dart';
+export 'src/block_resource_loading.dart';
+export 'src/elem_hide.dart';
